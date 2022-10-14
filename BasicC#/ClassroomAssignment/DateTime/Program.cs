6926210d8a8202293de7a57176985596a1dc4c4c
Gilbertmanoj R <gilbertmanoj.sf3971@gmail.com>
2022-10-14 08:59:16 +0530

    classroom assignment added

---
+++ BasicC#/ClassroomAssignment/DateTime/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace Datetime;
+class Program{
+    public static void Main(string[] args)
+    {
+        System.Console.WriteLine("\n");
+        DateTime doy=new DateTime(2021,8,10,10,40,32);
+        System.Console.WriteLine($"The year is : {doy.Year}");
+        System.Console.WriteLine($"The Month is : {doy.Month}");
+        System.Console.WriteLine($"The Day is : {doy.Day}");
+        System.Console.WriteLine($"The Hour is : {doy.Hour}");
+        System.Console.WriteLine($"The Minute is : {doy.Minute}");
+        System.Console.WriteLine($"The Second is : {doy.Second}");
+
+
+        
+        string format=doy.ToString("yyyy/MM/dd HH:mm:ss:tt");
+        string[] splitTime=format.Split(new string[]{"/",":"," "},StringSplitOptions.None);
+        System.Console.WriteLine("\n");
+        for(int i=splitTime.Length-1;i>=0;i--)
+        {
+            System.Console.Write($"{splitTime[i]} ");
+        }
+
+        System.Console.WriteLine("\n");
+        Console.WriteLine("\nEnter the time like this yyyy/MM/dd hh:mm:ss tt format: ");
+        DateTime userInput = DateTime.ParseExact(Console.ReadLine(), "yyyy/MM/dd hh:mm:ss tt", null);
+        Console.WriteLine(userInput.ToString("yyy/MM/dd"));
+    }
+}
---
+++ BasicC#/ClassroomAssignment/DoWhileLoop/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+namespace DoWhileLoop;
+class Program{
+    public static void Main(string[] args)
+    {
+        System.Console.Write("Enter the user input: ");
+        int num=int.Parse(Console.ReadLine());
+        string input=""; 
+        do
+        {
+            if(num%2!=0)
+            {
+                System.Console.WriteLine("Odd");
+            }
+            else
+            {
+                System.Console.WriteLine("Even");
+
+            }
+
+            System.Console.Write("You want to check with any other number? ");
+            input=Console.ReadLine();
+
+            while(true){
+                if(input=="yes" || input=="no")
+                {
+                    break;
+                
+                }
+                else{
+                    System.Console.WriteLine("Input is wrong.Enter the valid input !");
+                    System.Console.Write("You want to check with any other number? ");
+                    input=Console.ReadLine();
+                }
+
+
+            }
+            System.Console.Write("Enter the user input: ");
+            num=int.Parse(Console.ReadLine());    
+        }while( input=="yes");
+    }
+}
